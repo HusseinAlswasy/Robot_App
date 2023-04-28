936cbcbaf0ca677b2ebf72958ae1ec362de68655
Hussein Alswasy <01096694296hud@gmail.com>
2023-04-28 17:54:18 +0300

    first commit

---
+++ lib/screen/Interface_page1.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Interface1 extends StatelessWidget {
+  const Interface1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('fdf'),
+    );
+  }
+}
